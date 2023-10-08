f3f2f41a1b3e386b3ef8880855f3918407251b09
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2023-10-08 22:25:30 +0100

    Initial commit

---
+++ vue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from './components/navbar.vue'
+import Search from './components/Search.vue'
+</script>
+
+<template>
+  <header>
+    <NavBar/>
+  </header>
+  <Search />
+</template>
+
+<style scoped>
+</style>
