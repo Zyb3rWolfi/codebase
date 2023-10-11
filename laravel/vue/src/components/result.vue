cee3dc042f747b5c487551bf2958c4602c9bb6eb
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2023-10-11 23:03:35 +0100

    Added a copy feature

--- laravel/vue/src/components/result.vue
+++ laravel/vue/src/components/result.vue
@@ -20,16 +20,22 @@
     router.push("/")
   }
 
+  async function copyCode() {
+    await navigator.clipboard.writeText(codess);
+    alert("Copied to clipboard!");
+  }
+
 </script>
 
 <template>
 
-  <div style="background-color: #161616;" class="container grid grid-rows-3">
-    <p class=" text-2xl mt-2 font-semibold">{{props.search["strings"]}}</p>
+  <div style="background-color: #161616;" class="block grid-rows-3">
+    <p class=" text-2xl mt-2 font-semibold row-start-1">{{props.search["strings"]}}</p>
     
-    <div class="h-24 text-left">
-        <highlightjs :code="codess" autodetect/>     
+    <div class="h-24 text-left row-start-2 flex items-center justify-center ">
+        <highlightjs :code="codess" autodetect class="h-20 w-96"/>     
     </div>
+    <button type="button" @click="copyCode()" class=" mt-24 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm w-20 h-10 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Copy</button>
   </div>
 
 </template>
