1713273ad08b6dd8863eeea6fa56099bb5bef376
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2023-11-17 19:52:23 +0000

    yes

--- laravel/vue/src/main.js
+++ laravel/vue/src/main.js
@@ -1,17 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
-import './atom-one-dark.css'
-import './index.css'
+import './css/style.css'
+import './css/atom-one-dark.css'
+import './css/index.css'
 import router from '../src/router/index.js'
 import App from './App.vue'
 import 'highlight.js/styles/stackoverflow-light.css'
 import 'highlight.js/lib/common';
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 import VueCookies from 'vue-cookies';
-import axios from 'axios';
 import store from './store'
-import { install as VueMonacoEditorPlugin } from '@guolao/vue-monaco-editor'
-import { Modal} from 'flowbite';
 
 
 createApp(App)
@@ -19,12 +16,6 @@ createApp(App)
     .use(router)
     .use(VueCookies)
     .use(store)
-    .use(VueMonacoEditorPlugin, {
-        paths: {
-          // The recommended CDN config
-          vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.38.0/min/vs'
-        },
-      })
     .mount('#app')
     
 
