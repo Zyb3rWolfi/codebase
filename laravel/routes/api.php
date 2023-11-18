72b06352331f8fc13eaea3d161e11bca88d25c95
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2023-11-18 15:21:33 +0000

    sdsd

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -26,7 +26,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('changePassword', [App\Http\Controllers\AuthController::class, 'changePassword']);
     Route::post('logout', [App\Http\Controllers\AuthController::class, 'Logout']);
     Route::get('/test/{search}', [App\Http\Controllers\DbController::class, 'getData']);
-    Route::post('/addBlock', [App\Http\Controllers\DbController::class, 'addBlock']);
-    Route::post('/deleteBlock', [App\Http\Controllers\DbController::class, 'deleteBlock']);
-    Route::post('/updateBlock', [App\Http\Controllers\DbController::class, 'updateBlock']);
+    Route::middleware(['throttle:10,1']) ->group(function () {
+        Route::post('/addBlock', [App\Http\Controllers\DbController::class, 'addBlock']);
+        Route::post('/deleteBlock', [App\Http\Controllers\DbController::class, 'deleteBlock']);
+        Route::post('/updateBlock', [App\Http\Controllers\DbController::class, 'updateBlock']);
+    });
 });
