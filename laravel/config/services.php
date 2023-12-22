be1d0672d3a0c3aa64915714a633a0add1c3e5b2
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2023-12-22 00:38:22 +0000

    yes

--- laravel/config/services.php
+++ laravel/config/services.php
@@ -33,13 +33,13 @@ return [
     'google' => [
         'client_id' => '339296822545-qun0k9gtp3mbq3e70d9ib82mh7j0c3lk.apps.googleusercontent.com',
         'client_secret' => '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',
-        'redirect' => 'http://localhost:8000/auth/google/callback',
+        'redirect' => 'http://codebranch/auth/google/callback',
     ],
 
     'github' => [
         'client_id' => 'Iv1.facd4d85531bae65',
         'client_secret' => '4308c4f49585df5f01942e09d26f4f222a142b17',
-        'redirect' => 'http://localhost:8000/auth/github/callback',
+        'redirect' => 'http://codebranch.me/auth/github/callback',
     ]
 
 ];
